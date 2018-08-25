7d1c76b7ead55f3a018a2ab4a17535e395d70ace
Lindell Carter <lindellcarternyc@gmail.com>
2018-08-25 13:11:54 -0400

    create request header microservice

--- server.js
+++ server.js
@@ -19,11 +19,15 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
+// Request header microserveice
+app.get('/api/whoami', (req, res) => {
+  
+  const { ip, headers } = req
+  const language = headers['accept-language']
+  const software = headers['user-agent']
+  
+  res.json({ipaddress: ip, language, software})
+})
 
 // Configure port
 const PORT = process.env.PORT || 8869
